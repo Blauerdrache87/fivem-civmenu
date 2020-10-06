2d5499513b537436a632acb8d44bbe8a33f48ad7
Marcel <67207947+szymczakovv@users.noreply.github.com>
2020-10-06 23:25:16 +0200

    Update __resource.lua

--- dowod/__resource.lua
+++ dowod/__resource.lua
@@ -1,5 +1,7 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-client_script 'cl.lua'
-
+client_scripts {
+      'cl.lua',
+      '@mysql-async/lib/MySQL.lua'
+  }
 server_script 'sv.lua'
